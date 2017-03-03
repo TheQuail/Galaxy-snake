a37400538bc6f21fe2109c43c6044fa8b4957553
Struggler <xyhstruggler@gmail.com>
2017-03-03 14:08:44 -0600

    asdasd

--- main/index/index.js
+++ main/index/index.js
@@ -1,8 +1,13 @@
 var container;
 var camera, scene, renderer;
 var geometry, group;
-var mouseX = 10000,
-    mouseY = 10000;
+var mouseX = 0,
+    mouseY = 0;
+var mouseXOnMouseDown = 0;
+var targetRotation = 0,
+    targetRotationY = 0;
+var targetRotationOnMouseDown = 0;
+
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 /**
@@ -27,6 +32,8 @@ render();
 gui.add(camera.position, 'x', -2000, 2000);
 gui.add(camera.position, 'y', -2000, 2000);
 gui.add(camera.position, 'z', -2000, 2000);
+gui.add(group.position, 'y', -2000, 2000);
+
 // gui.add(camera, 'bouncingSpeed', -2000, 0.5);
 
 function init() {
@@ -40,20 +47,22 @@ function init() {
     scene.fog = new THREE.Fog(0xffffff, 1, 10000);
     var geometry = new THREE.BoxGeometry(100, 100, 100);
     group = new THREE.Group();
-
-    for(var k = 0; k < 10; k++){
-      for(var j = 0; j < 10; j++){
-        for(var i = 0; i < 10; i++){
-          var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({wireframe: true,color: 0x0000FF}));
-          mesh.position.x = 0.5 * 2000 - 100 * i;
-          mesh.position.y = 0.5 * 2000 - 100 * j;
-          mesh.position.z = 0.5 * 2000 - 100 * k;
-          mesh.matrixAutoUpdate = false;
-          mesh.updateMatrix();
-          group.add(mesh);
+    for (var k = 0; k < 10; k++) {
+        for (var j = 0; j < 10; j++) {
+            for (var i = 0; i < 10; i++) {
+                var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+                    wireframe: true,
+                    color: 0x0000FF
+                }));
+                mesh.position.x = 0.5 * 2000 - 100 * i;
+                mesh.position.y = 0.5 * 2000 - 100 * j;
+                mesh.position.z = 0.5 * 2000 - 100 * k;
+                mesh.matrixAutoUpdate = false;
+                mesh.updateMatrix();
+                group.add(mesh);
+            }
         }
     }
-
 
     scene.add(group);
     renderer = new THREE.WebGLRenderer();
@@ -62,6 +71,9 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.sortObjects = false;
     container.appendChild(renderer.domElement);
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    document.addEventListener('touchstart', onDocumentTouchStart, false);
+    document.addEventListener('touchmove', onDocumentTouchMove, false);
     window.addEventListener('resize', onWindowResize, false);
 }
 
@@ -73,9 +85,46 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function onDocumentMouseDown(event) {
+    event.preventDefault();
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    document.addEventListener('mouseout', onDocumentMouseOut, false);
+    mouseXOnMouseDown = event.clientX - windowHalfX;
+    targetRotationOnMouseDown = targetRotation;
+}
+
 function onDocumentMouseMove(event) {
-    // mouseX = (event.clientX - windowHalfX) * 10;
-    // mouseY = (event.clientY - windowHalfY) * 10;
+    mouseX = event.clientX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
+}
+
+function onDocumentMouseUp(event) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, false);
+    document.removeEventListener('mouseup', onDocumentMouseUp, false);
+    document.removeEventListener('mouseout', onDocumentMouseOut, false);
+}
+
+function onDocumentMouseOut(event) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, false);
+    document.removeEventListener('mouseup', onDocumentMouseUp, false);
+    document.removeEventListener('mouseout', onDocumentMouseOut, false);
+}
+
+function onDocumentTouchStart(event) {
+    if (event.touches.length === 1) {
+        event.preventDefault();
+        mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
+        targetRotationOnMouseDown = targetRotation;
+    }
+}
+
+function onDocumentTouchMove(event) {
+    if (event.touches.length === 1) {
+        event.preventDefault();
+        mouseX = event.touches[0].pageX - windowHalfX;
+        targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
+    }
 }
 
 function animate() {
@@ -111,17 +160,19 @@ function render(target) {
     // camera.position.x -= 2;
     // camera.position.z -= 2;
     // camera.position.y += 100;
+<<<<<<< Updated upstream
     camera.lookAt(cameratarget);
     // console.log(cameratarget);
-    renderer.render(scene, camera);
-<<<<<<< HEAD
-}
-function change_camera() {
-  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.z = 2000;
-  camera.position.x = 2000;
-  camera.position.y = 2000;
-}
 =======
+    group.rotation.x += ( targetRotation - group.rotation.x ) * 0.05;
+>>>>>>> Stashed changes
+    renderer.render(scene, camera);
 }
->>>>>>> origin/master
+
+
+function change_camera() {
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 2000;
+    camera.position.x = 2000;
+    camera.position.y = 2000;
+}
